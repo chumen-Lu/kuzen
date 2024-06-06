337ac7609240f0d6e41cfdb2d0f5ef2cc314fc9f
kuzen <xiaokundaa@gmail.com>
2024-06-06 14:02:21 +0800

    chore: 添加手动加载路由

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import './assets/main.css'
+// 取消全局ant-design引入,使用按需引入
 // import Antd from 'ant-design-vue'
 // import 'ant-design-vue/dist/reset.css'
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
 
 const app = createApp(App)
+// 取消全局ant-design引入,使用按需引入
 // app.use(Antd)
+app.use(router)
 app.mount('#app')
